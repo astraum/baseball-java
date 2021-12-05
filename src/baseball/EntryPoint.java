93ebcea8dd2d4506792041d0b2f80f44a572c438
Seung Wan Jeong <sjeong4776@gmail.com>
2021-12-06 03:00:38 +0900

    Simplified the code

---
+++ src/baseball/EntryPoint.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class EntryPoint {
+    public static void main(String[] args) {
+
+        Game game = new Game();
+        game.initialize();
+        game.start();
+
+    }
+}
--- src/baseball/Game.java
+++ src/baseball/Game.java
@@ -1,29 +1,56 @@
 package baseball;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Game {
 
     private static final int NUMBER_OF_BALLS = 3;
 
     private final Scanner scanner = new Scanner(System.in);
-    private final Target target;
-    private Score lastScore;
+    private String target = "";
+    private int strike = 0;
+    private int ball = 0;
 
-    public Game() {
-        target = new Target(NUMBER_OF_BALLS);
+    public void initialize() {
+        List<String> pool = IntStream.rangeClosed(1, 9).mapToObj(String::valueOf).collect(Collectors.toList());
+        Collections.shuffle(pool);
+        target = String.join("", pool.subList(0, NUMBER_OF_BALLS));
     }
 
-    public void playBall() {
+    public void start() {
 
+        System.out.println("숫자야구 게임에 오신 것을 환영합니다.");
+
+        try (scanner) {
+            while (strike != NUMBER_OF_BALLS) {
+                strike = 0;
+                ball = 0;
+                tryGuessing();
+                printResult();
+            }
+        } finally {
+            System.out.println("정답입니다!");
+            System.out.println("게임을 종료합니다.");
+        }
+
+    }
+
+    private void tryGuessing() {
         String guess = readUserInput();
-        Pitch pitch = new Pitch(guess);
-        lastScore = new Score(pitch, target);
-        System.out.println(lastScore);
-    }
 
-    public boolean isWon() {
-        return lastScore.getStrike() == NUMBER_OF_BALLS;
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
+
+            if (guess.charAt(i) == target.charAt(i)) {
+                strike += 1;
+            } else if (target.indexOf(guess.charAt(i)) != -1) {
+                ball += 1;
+            }
+        }
+
     }
 
     private String readUserInput() {
@@ -31,8 +58,18 @@ public class Game {
         while (true) {
             System.out.printf("0이 포함되지 않은 %d자리 정수를 입력하세요: ", NUMBER_OF_BALLS);
             String input = scanner.nextLine();
-            if (input.matches("[1-9]{" + NUMBER_OF_BALLS + "}")) return input;
+            if (input.matches("[1-9]{" + NUMBER_OF_BALLS + "}")) {
+                return input;
+            }
             System.out.print("잘못된 입력입니다. ");
+        }
+    }
+
+    private void printResult() {
+        if (strike != 0 || ball != 0) {
+            System.out.printf("%d 스트라이크, %d 볼%n", strike, ball);
+        } else {
+            System.out.println("낫싱");
         }
     }
 
